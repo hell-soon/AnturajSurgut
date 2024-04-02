40e04f5a5e6a4405474e2f3cbd9e86b16687ca24
WinerTy <luchiy.v.mire@mail.ru>
2024-04-02 12:57:48 +0700

    FIX: Docker

--- backend/start.sh
+++ backend/start.sh
@@ -5,8 +5,5 @@ echo Starting Gunicorn.
 exec gunicorn backend.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 1 \
-<<<<<<< HEAD
     --chdir backend
-=======
-    --chdir backend
->>>>>>> db58ad547cd72490e0bb3be125e907c615ceb510
+
