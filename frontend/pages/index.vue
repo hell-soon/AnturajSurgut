e4b830ed917d5526a8db2b2eab69e3a6e28df73f
Maksim1-smg <pmaks2365@gmail.com>
2024-04-02 16:37:10 +0700

    feat: nuxt

---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <NuxtLink to="/">Learn more about Nuxt Routing</NuxtLink>
+  </div>
+</template>
