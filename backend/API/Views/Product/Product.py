8c62cb8ebfacf53fd4d9fb2e7c3cba70b5dbaa71
WinerTy <nikitokmay@gmail.com>
2024-04-24 17:58:40 +0700

    Fix

--- backend/API/Views/Product/Product.py
+++ backend/API/Views/Product/Product.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle
-from rest_framework.decorators import action, permission_classes
+from rest_framework.decorators import action
 
 from django_filters import rest_framework as filters
 
