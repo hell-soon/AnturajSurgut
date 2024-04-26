337283cc0f6599273e388141d041ff1f932aed22
WinerTy <nikitokmay@gmail.com>
2024-04-26 23:35:50 +0700

    Code Review

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
+from .Views.Catalogs import CatalogViewSet
+from .Views.Tags import TagsViewSet
+from .Views.SubCatalogs import SubCatalogViewSet
 from .Views.Product import ProductViewSet
 from .Views.Search import GlobalSearch
 from .Views.Filters import FilterMenu
-from .Routers.routers import router
 
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("catalog/", CatalogViewSet.as_view({"get": "list"})),
+    path("subcatalog/", SubCatalogViewSet.as_view({"get": "list"})),
+    path("tags/", TagsViewSet.as_view({"get": "list"})),
     path("list/", ProductViewSet.as_view({"get": "list"})),
     path("list/<int:pk>/", ProductViewSet.as_view({"get": "info"})),
     path("search/", GlobalSearch.as_view()),
