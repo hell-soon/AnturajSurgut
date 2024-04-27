de5a663102ebb13530238e49f8ebbca0b9b25850
WinerTy <nikitokmay@gmail.com>
2024-04-27 12:14:21 +0700

    Code Review

--- backend/API/Views/Search.py
+++ backend/API/Views/Search.py
@@ -8,6 +8,7 @@ from drf_yasg.utils import swagger_auto_schema
 
 from API.serializers.SearchSerializers import SearchSerializer
 from API.serializers.ProductSerializers import ProductSerializer
+from API.serializers.Schemas import ProductPaginationSerializer
 from backend.paginator import StandardResultsSetPagination
 from DB.models import Product
 
@@ -18,20 +19,20 @@ class GlobalSearch(APIView):
     # throttle_classes = [SearchThrottle] FIXME
 
     @swagger_auto_schema(
-        request_body=SearchSerializer, responses={200: ProductSerializer(many=True)}
+        request_body=SearchSerializer,
+        responses={200: ProductPaginationSerializer(many=True)},
+        pagination_class=StandardResultsSetPagination,
     )
     def post(self, request):
         search_serializer = SearchSerializer(data=request.data)
-        if search_serializer.is_valid(raise_exception=True):
-            search_data = search_serializer.data.get("search_data")
-            products = (
-                Product.objects.filter(product_status=True).filter(
-                    Q(name__icontains=search_data)
-                    | Q(description__icontains=search_data)
-                )
-            ).order_by("-created_at")
-            paginator = StandardResultsSetPagination()
-            page = paginator.paginate_queryset(products, request)
-            serialized_products = ProductSerializer(page, many=True)
-            return paginator.get_paginated_response(serialized_products.data)
-        return Response(search_serializer.errors, status=400)
+        search_serializer.is_valid(raise_exception=True)
+        search_data = search_serializer.data.get("search_data")
+        products = (
+            Product.objects.filter(product_status=True).filter(
+                Q(name__icontains=search_data) | Q(description__icontains=search_data)
+            )
+        ).order_by("-created_at")
+        paginator = StandardResultsSetPagination()
+        page = paginator.paginate_queryset(products, request)
+        serialized_products = ProductSerializer(page, many=True)
+        return paginator.get_paginated_response(serialized_products.data)
