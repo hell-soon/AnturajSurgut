64cba78cc03c3e610739e5f6dc95ea975044a647
WinerTy <nikitokmay@gmail.com>
2024-04-28 21:21:36 +0700

    Fix

--- backend/DB/Tasks/Email/OrderEmails/confirm_email.py
+++ backend/DB/Tasks/Email/OrderEmails/confirm_email.py
@@ -7,12 +7,10 @@ from ..send_html import send_html_email
 @shared_task
 def send_order_confirmation_email(pk):
     order = Order.objects.get(pk=pk)
-    address = OrderAddress.objects.get(order=order).__str__()
     recipient_list = [order.user_email]
     data = {
         "initials": order.user_initials,
         "order_number": order.order_number,
-        "order_address": address,
         "order_status": order.order_status.name,
         "order_comment": order.comment,
         "order_paymant": order.order_paymant,
